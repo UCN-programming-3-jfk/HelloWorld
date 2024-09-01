c656a8165224f59b9c37a6f536bfc9b34d58cc34
Jakob Farian Krarup <kr@rup.dk>
2024-09-01 11:12:38 +0200

    Add project files.

---
+++ HelloWorld/Greeter.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Class which generates greetings
+    /// </summary>
+    public class Greeter
+    {
+        public string MyOwnName { get; set; }
+        public Greeter(string myOwnName = null)
+        {
+            MyOwnName = myOwnName;
+        }
+        public string HelloCSharpWorld()
+        {
+            return "Hello C# World!";
+        }
+
+        public string WelcomeUserToCSharp(string name)
+        {
+            return $"Hello {name}, and welcome to C#";
+        }
+
+        public string[] GreetEverybody(string[] names)
+        {
+            string[] greetings = new string[names.Length];
+
+            for (int nameCounter = 0; nameCounter < names.Length; nameCounter++)
+            {
+                    greetings[nameCounter] = $"Hello, {names[nameCounter]}";
+            }
+            return greetings;
+        }
+
+        public string[] GreetEverybodyAndMePersonally(string[] names)
+        {
+            string[] greetings = new string[names.Length];
+
+            for (int nameCounter = 0; nameCounter < names.Length; nameCounter++)
+            {
+                string name = names[nameCounter];
+                if (name == MyOwnName)
+                {
+                    greetings[nameCounter] = $"Welcome back, {name}";
+                }
+                else
+                {
+                    greetings[nameCounter] = $"Hello, {name}";
+                }
+            }
+            return greetings;            
+        }
+
+        public string[] GreetEverybodyAndMePersonallyConciseCode(string[] names)
+        {
+            List<string> greetings = new List<string>();
+
+            foreach (var name in names)
+            {
+                //Ternary operator:
+                //https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/conditional-operator
+                greetings.Add(name == MyOwnName ? $"Welcome back {name}" : $"Hello, {name}");
+            }
+            return greetings.ToArray();
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        //All non-UI functionality moved into this class
+        private static Greeter _greeter = new Greeter("Joan");
+
+        private static void Main(string[] args)
+        {
+            Exercise_1_2();
+            Exercise_1_4();
+            Exercise_1_9(args);
+            Exercise_1_10(args);
+
+        }
+        private static void Exercise_1_2()
+        {
+            Console.WriteLine(_greeter.HelloCSharpWorld());
+        }
+
+        private static void Exercise_1_4()
+        {
+            Console.WriteLine("Enter your name");
+            string username = Console.ReadLine();
+            Console.WriteLine(_greeter.WelcomeUserToCSharp(username));
+        }
+
+        private static void Exercise_1_9(string[] args)
+        {
+            if (args.Length == 0) { WriteInstructions(); return; }
+            foreach (var arg in _greeter.GreetEverybodyAndMePersonally(args))
+            {
+                Console.WriteLine(arg);
+            }
+        }
+
+        private static void Exercise_1_10(string[] names)
+        {
+            if (names.Length == 0) { WriteInstructions(); return; }
+            _greeter.GreetEverybodyAndMePersonally(names);
+        }
+        private static void WriteInstructions()
+        {
+            Console.WriteLine("Hello World greeter program");
+            Console.WriteLine("Usage: HelloWorld.exe [name 1] [name 2] ... [name n]");
+            Console.WriteLine("Example use: HelloWorld.exe Ian Janet John Barbara");
+        }
+    }
+}
